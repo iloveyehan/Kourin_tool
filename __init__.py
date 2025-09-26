173eb407ee566e0732ee6170c0032400d92edf75
iloveyehan <649730016@qq.com>
2025-09-26 18:45:20 +0800

    1

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ bl_info = {
     "name": "Kourin_tool",
     "author": "Cupcko[649730016@qq.com]",
     "blender": (3, 5, 0),
-    "version": (1, 3, 1),
+    "version": (1, 3, 2),
     "description": "This is a template for building addons",
     "warning": "",
     "doc_url": "[documentation url]",
--- update_version.py
+++ update_version.py
@@ -3,7 +3,7 @@ import json
 import re
 
 # 定义新版本号（字符串形式）
-version = '1.3.1'
+version = '1.3.2'
 
 def update_json():
     json_file = 'version.json'
